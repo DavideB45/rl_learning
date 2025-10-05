b74b9626f8473e3c82d93af5108946575f1a2eb7
DavideB45 <davide@pop-os.station>
2025-10-05 12:59:48 +0200

    working PPO

--- Cartpole/ppo.py
+++ Cartpole/ppo.py
@@ -1,20 +1,21 @@
 import gymnasium as gym
 
-from stable_baselines3.sac.policies import MlpPolicy
+from stable_baselines3.ppo.policies import MlpPolicy
 from stable_baselines3.ppo import PPO
 
 env = gym.make('CartPole-v1')
 
 model = PPO(MlpPolicy, env, verbose=1)
 # Train the agent
-model.learn(total_timesteps=10000)
+model.learn(total_timesteps=30000)
 
 # Enjoy trained agent
-obs = env.reset()
+env = gym.make('CartPole-v1', render_mode="human")
+obs, info = env.reset()
 for i in range(1000):
     action, _states = model.predict(obs, deterministic=False)
-    obs, reward, done, info = env.step(action)
+    obs, reward, terminated, truncated, info = env.step(action)
     env.render()
-    if done:
-        obs = env.reset()
+    if terminated or truncated:
+        obs, info = env.reset()
 env.close()
