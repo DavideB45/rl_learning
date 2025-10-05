57a0b2141cc29ba3a0e806f14dc8deef2428329c
DavideB45 <d.borghini3@studenti.unipi.it>
2025-10-05 19:48:40 +0200

    evaluation

--- Cartpole/ppo.py
+++ Cartpole/ppo.py
@@ -3,11 +3,22 @@ import gymnasium as gym
 from stable_baselines3.ppo.policies import MlpPolicy
 from stable_baselines3.ppo import PPO
 
+from helpers import evaluate_policy
+
 env = gym.make('CartPole-v1')
 
-model = PPO(MlpPolicy, env, verbose=1)
+model = PPO(MlpPolicy, env, verbose=0)
+
+# Evaluate untrained agent
+mean_reward, std_reward = evaluate_policy(model, env)
+print(f"Mean reward (untrained): {mean_reward:.2f} +/- {std_reward:.2f}")
+
 # Train the agent
-model.learn(total_timesteps=30000)
+model.learn(total_timesteps=20000, progress_bar=True)
+
+# Evaluate the agent
+mean_reward, std_reward = evaluate_policy(model, env)
+print(f"Mean reward: {mean_reward:.2f} +/- {std_reward:.2f}")
 
 # Enjoy trained agent
 env = gym.make('CartPole-v1', render_mode="human")
