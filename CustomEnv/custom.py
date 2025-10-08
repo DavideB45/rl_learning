de3a9b099717388b8de44de648aff2524ecc03d6
DavideB45 <d.borghini3@studenti.unipi.it>
2025-10-08 19:30:51 +0200

    Environment in the making

---
+++ CustomEnv/custom.py
@@ -0,0 +1,3 @@
+# this is a link for a tutotial for callbacks
+# https://colab.research.google.com/github/araffin/rl-tutorial-jnrr19/blob/sb3/4_callbacks_hyperparameter_tuning.ipynb#scrollTo=DN05_Io8fMAr
+
