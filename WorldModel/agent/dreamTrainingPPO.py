a31ff257452c5ed57a68115a30b467ba70fd027b
DavideB45 <dadewww@gmail.com>
2025-11-01 13:31:45 +0100

    multiple it

--- WorldModel/agent/dreamTrainingPPO.py
+++ WorldModel/agent/dreamTrainingPPO.py
@@ -33,31 +33,32 @@ if __name__ == "__main__":
 	if not os.path.exists(CURRENT_ENV['data_dir'] + PPO_MODEL + ".zip"):
 		raise FileNotFoundError(f"PPO model not found at {CURRENT_ENV['data_dir'] + PPO_MODEL + '.zip'}. Please train the PPO model first.")
 
-	# Make some new experience in the pseudo-dream environment
-	experience_env = PseudoDreamEnv(CURRENT_ENV, render_mode="rgb_array")
-	policy = PPO.load(CURRENT_ENV['data_dir'] + PPO_MODEL + ".zip", env=experience_env)
-	images, history = make_experience(experience_env, policy, n_steps=EXPERIENCE_STEPS)
+	for iteration in range(ITERATIONS):
+		# Make some new experience in the pseudo-dream environment
+		experience_env = PseudoDreamEnv(CURRENT_ENV, render_mode="rgb_array")
+		policy = PPO.load(CURRENT_ENV['data_dir'] + PPO_MODEL + ".zip", env=experience_env)
+		images, history = make_experience(experience_env, policy, n_steps=EXPERIENCE_STEPS)
 
-	# Now use the experience to finetune the VAE and MDNRNN models
-	vae = VAE()
-	vae.load_state_dict(torch.load(CURRENT_ENV['data_dir'] + VAE_MODEL, map_location=torch.device('cpu')))
-	train_vae(vae_=vae, images=images, epochs=TUNING_EPOCHS_VAE)
-	mdrnn = MDNRNN()
-	mdrnn.load_state_dict(torch.load(CURRENT_ENV['data_dir'] + MDRNN_MODEL, map_location=torch.device('cpu')))
-	train_mdrnn(mdrnn_=mdrnn, data_=history, epochs=TUNING_EPOCHS_MDRNN)
-	torch.save(vae.state_dict(), CURRENT_ENV['data_dir'] + VAE_MODEL)
-	torch.save(mdrnn.state_dict(), CURRENT_ENV['data_dir'] + MDRNN_MODEL)
+		# Now use the experience to finetune the VAE and MDNRNN models
+		vae = VAE()
+		vae.load_state_dict(torch.load(CURRENT_ENV['data_dir'] + VAE_MODEL, map_location=torch.device('cpu')))
+		train_vae(vae_=vae, images=images, epochs=TUNING_EPOCHS_VAE)
+		mdrnn = MDNRNN()
+		mdrnn.load_state_dict(torch.load(CURRENT_ENV['data_dir'] + MDRNN_MODEL, map_location=torch.device('cpu')))
+		train_mdrnn(mdrnn_=mdrnn, data_=history, epochs=TUNING_EPOCHS_MDRNN)
+		torch.save(vae.state_dict(), CURRENT_ENV['data_dir'] + VAE_MODEL)
+		torch.save(mdrnn.state_dict(), CURRENT_ENV['data_dir'] + MDRNN_MODEL)
 
-	# Finally, we can retrain the PPO model in the dream environment with the finetuned world models
-	dream_env = DreamEnv(CURRENT_ENV, render_mode="rgb_array")
-	eval_callback = EvalCallback(experience_env, eval_freq=15000, n_eval_episodes=5)
-	policy.learn(total_timesteps=TUNING_PPO_TIMESTEPS, callback=eval_callback)
-	policy.save(CURRENT_ENV['data_dir'] + PPO_MODEL)
-	print("Finished dream tuning PPO training.")
+		# Finally, we can retrain the PPO model in the dream environment with the finetuned world models
+		dream_env = DreamEnv(CURRENT_ENV, render_mode="rgb_array")
+		eval_callback = EvalCallback(experience_env, eval_freq=15000, n_eval_episodes=5)
+		policy.learn(total_timesteps=TUNING_PPO_TIMESTEPS, callback=eval_callback, progress_bar=True)
+		policy.save(CURRENT_ENV['data_dir'] + PPO_MODEL)
+		print("Finished dream tuning PPO training.")
 
-	#clean up
-	experience_env.close()
-	dream_env.close()
-	del experience_env, dream_env
-	del policy, vae, mdrnn
-	del images, history
+		#clean up
+		experience_env.close()
+		dream_env.close()
+		del experience_env, dream_env
+		del policy, vae, mdrnn
+		del images, history
