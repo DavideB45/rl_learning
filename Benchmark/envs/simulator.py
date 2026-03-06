b10583442fee1b1d0382e633ffe511b3a8efb87b
Davide Borghini <dadewww@gmail.com>
2026-03-06 11:18:20 +0100

    META wrapper and simulator (from old code with small changes

---
+++ Benchmark/envs/simulator.py
@@ -0,0 +1,199 @@
+import cv2
+import numpy as np
+from gymnasium import spaces
+from stable_baselines3.common.vec_env import VecEnv
+import torch
+from stable_baselines3.ppo import PPO
+from PIL import Image
+from torch.utils.data import DataLoader
+import time
+
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '../'))
+
+from helpers.data import get_data_path, make_seq_dataloader_safe
+from vae.vqVae import VQVAE
+from dynamics.lstmc import LSTMQClass
+from helpers.model_loader import load_vq_vae, load_lstm_quantized
+from helpers.general import best_device
+from global_var import *
+
+class MetaDreamEnv(VecEnv):
+	"""
+	Completely simulated environment using the VAE and MDRNN models
+	The starting state is obtained from the real environment
+	Then the environment is simulated using only the world model
+	"""
+
+
+	def __init__(self, vq:VQVAE, lstm:LSTMQClass, dataloader:DataLoader, init_len:int=1, ep_len:int=20, num_envs: int = 1):
+		
+		self.num_envs = num_envs
+		self.max_len = ep_len # this way the model will learn only 20 steps, hopefully in the end he will manage to merge his knowledge
+		self.step_count = 0
+		self.i_len = init_len
+
+		self.vq = vq
+		self.vq.eval()
+		self.vq_dim = self.vq.latent_dim**2*self.vq.code_depth
+		self.lstm = lstm
+		self.lstm.eval()
+		self.hidden_state = None # (num_envs, hidden_dim)
+
+		self.observation_space = spaces.Box(
+			low=-np.inf, high=np.inf, 
+			shape=(self.vq_dim + self.lstm.hidden_dim,), 
+			dtype=np.float32
+		)
+		self.action_space = spaces.Box(
+			low=-1, high=1, 
+			shape=(4,),
+			dtype=np.float32
+		)
+		super(MetaDreamEnv, self).__init__(
+			num_envs=num_envs,
+			action_space=self.action_space,
+			observation_space=self.observation_space	
+		)
+
+		self.data = dataloader
+
+	def reset(self, seed=None, options=None):
+		'''
+		Reset the environment
+		seed: random seed
+		options: additional options
+		returns: initial observation (np.array) obtained encoding the first image and the initial hidden state
+		'''
+		if seed is not None:
+			print("[WARNING] I haven't implemented seed it's always random")
+		init_data_list = [self.data.dataset[np.random.randint(len(self.data.dataset))] for _ in range(self.num_envs)]
+		with torch.no_grad():
+			latents = torch.stack([init_data['latent'][:self.i_len, :] for init_data in init_data_list]).to(self.vq.device)
+			actions = torch.stack([init_data['action'][:self.i_len, :] for init_data in init_data_list]).to(self.vq.device)
+			props = torch.stack([init_data['proprioception'][:self.i_len, :] for init_data in init_data_list]).to(self.vq.device)
+
+			_, pred, prop, _, h = self.lstm.forward(latents, actions, props, None)
+
+			self.hidden_state = h
+			self.current_latent = pred[:, -1, :, :, :]
+			self.current_prop = prop[:, -1, :]
+			latent_flat = self.current_latent.reshape(self.num_envs, -1)
+			hidden_flat = self.hidden_state[0].reshape(self.num_envs, -1)
+
+			representation = torch.cat([latent_flat, hidden_flat], dim=-1).cpu().numpy()
+		self.step_count = 0
+		return representation
+
+	def step(self, actions) -> tuple:
+		'''
+		Step in the environment using only MDRNN
+		action: action to take
+		returns: observation (np.array), reward (float), terminated (bool), truncated (bool), info (dict)
+		'''
+		if actions.ndim == 1:
+			actions = actions[np.newaxis, :]
+		with torch.no_grad():
+			action_tensor = torch.tensor(actions, dtype=torch.float32).unsqueeze(1).to(self.vq.device)
+			latent_input = self.current_latent.unsqueeze(1).to(self.vq.device)
+			prop_input = self.current_prop.unsqueeze(1).to(self.vq.device)
+			_, pred, prop, rew, h = self.lstm.forward(latent_input, action_tensor, prop_input, self.hidden_state)
+
+			self.step_count += 1
+			self.hidden_state = h
+			self.current_latent = pred[:, -1, :, :, :]
+			self.current_prop = prop[:, -1, :]
+
+			latent_flat = self.current_latent.reshape(self.num_envs, -1)
+			hidden_flat = self.hidden_state[0].reshape(self.num_envs, -1)
+			representation = torch.cat([latent_flat, hidden_flat], dim=-1).cpu().numpy()
+
+			terminateds = np.array([self.step_count >= self.max_len] * self.num_envs, dtype=bool)
+			infos = [
+				{'terminal_observation': representation[i]} for i in range(self.num_envs)
+			]
+		return (
+			representation if not terminateds.any() else self.reset(), # based on world model
+			np.array(rew.flatten().cpu()), # from world model
+			terminateds, # For now only based on step count
+			infos
+		)
+	
+	def render(self):
+		if(self.num_envs != 1):
+			print('[WARNING] trying to render vectorized env, you are not Doctor strange')
+			return
+		with torch.no_grad():
+			img = self.vq.decode(self.current_latent).squeeze(0).permute(1, 2, 0).cpu().numpy()
+			img = (img * 255).astype(np.uint8)
+			image = Image.fromarray(img)
+			image_resized = image.resize((512, 512), Image.NEAREST)
+			cv2.imshow('DreamEnv', np.array(image_resized))
+			cv2.waitKey(100)
+			return image_resized
+			#return img
+		
+	def close(self):
+		pass
+
+	# additional implementation for the interface
+	def env_is_wrapped(self, wrapper_class, indices = None):
+		return super().env_is_wrapped(wrapper_class, indices)
+	
+	def env_method(self, method_name, *method_args, indices = None, **method_kwargs):
+		return super().env_method(method_name, *method_args, indices=indices, **method_kwargs)
+	
+	def get_attr(self, attr_name, indices = None):
+		if 'render_mode':
+			return ['rgb_array' for _ in range(self.num_envs)]
+		return super().get_attr(attr_name, indices)
+	
+	def set_attr(self, attr_name, value, indices = None):
+		return super().set_attr(attr_name, value, indices)
+	
+	def step_async(self, actions):
+		# this is a trick: the call is not really async so this can cause some slowdown
+		self.async_values = self.step(actions)
+	
+	def step_wait(self):
+		return self.async_values
+	
+
+
+if __name__ == "__main__":
+	SMOOTH = True if SMOOTH > 0 else False
+	vq = load_vq_vae(CURRENT_ENV, CODEBOOK_SIZE, CODE_DEPTH, LATENT_DIM, True, SMOOTH, best_device())
+	lstm = load_lstm_quantized(CURRENT_ENV, vq, best_device(), HIDDEN_DIM, SMOOTH, False, False)
+	env = MetaDreamEnv(vq=vq, lstm=lstm, dataloader=make_seq_dataloader_safe(get_data_path(CURRENT_ENV['img_dir'], True, 0), vq, 100, 1), 
+					  num_envs=1, ep_len=500, init_len=1)
+	observation = env.reset()
+	frames = []
+	frames.append(env.render())
+	done = False
+	total_reward = 0
+	step_count = 0
+	#agent = PPO.load(PUSHER['models'] + 'agent', env)
+	while not done:
+		action = env.action_space.sample()  # random action
+		#action, _states = agent.predict(observation, deterministic=True)
+		observation, reward, terminated, info = env.step(action)
+		print(f"Step {step_count} Reward: {reward}")
+		frames.append(env.render())
+		done = terminated.any()
+		total_reward += reward
+		step_count += 1
+		#time.sleep(2)
+		if done:
+			print(f"Game over! Total Reward: {total_reward}")
+	env.close()
+
+	GIF_PATH = "output.gif"
+	FRAME_DURATION_MS = 50
+	# frames[0].save(
+	# 	GIF_PATH,
+	# 	save_all=True,
+	# 	append_images=frames[1:],
+	# 	loop=0,                    # 0 = loop forever
+	# 	duration=FRAME_DURATION_MS,
+	# )
--- Benchmark/envs/wrapper.py
+++ Benchmark/envs/wrapper.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import gymnasium as gym
 from gymnasium import spaces
+import metaworld
 import torch
 import torchvision.transforms as T
 import json
@@ -18,9 +19,10 @@ from vae.vqVae import VQVAE
 from dynamics.lstm import LSTMQuantized
 from helpers.model_loader import load_vq_vae, load_lstm_quantized
 from helpers.general import best_device
-from global_var import PUSHER
+from helpers.data import get_data_path
+from global_var import *
 
-class PusherWrapEnv(gym.Env):
+class MetaWrapEnv(gym.Env):
 	"""
 	This environemt is a wrapper of the real environment used at inference time
 	Since the agent can't do inference directly on the data coming from the environment
@@ -28,7 +30,7 @@ class PusherWrapEnv(gym.Env):
 
 
 	def __init__(self, vq:VQVAE=None, lstm:LSTMQuantized=None):
-		super(PusherWrapEnv, self).__init__()
+		super(MetaWrapEnv, self).__init__()
 
 		self.vq = vq
 		self.vq.eval()
@@ -36,14 +38,11 @@ class PusherWrapEnv(gym.Env):
 		self.lstm = lstm
 		self.lstm.eval()
 
-		self.env = gym.make('Pusher-v5', 
-			render_mode='rgb_array',
-			default_camera_config=PUSHER['default_camera_config'],
-		)
-		self.renderer = self.env.env.env.env.mujoco_renderer
+		self.env = gym.make('Meta-World/MT1', env_name=CURRENT_ENV['env_name'],
+				render_mode='rgb_array', camera_id=CURRENT_ENV['camera_id'], width = 128, height = 128)
 		self.action_space = spaces.Box(
 			low=-1, high=1, 
-			shape=(7,), 
+			shape=(4,), 
 			dtype=np.float32
 		)
 		self.observation_space = spaces.Box(
@@ -60,10 +59,8 @@ class PusherWrapEnv(gym.Env):
 		Returns:
 			Image.Image: resized image
 		'''
-		self.renderer.camera_id = 2
-		img = self.renderer.render(render_mode='rgb_array')
+		img = self.env.render()
 		img = Image.fromarray(img)
-		img = img.resize((64, 64))
 		return img
 	
 	def reset(self, seed=None, options=None):
@@ -83,11 +80,9 @@ class PusherWrapEnv(gym.Env):
 				 torch.zeros(1, 1, self.lstm.hidden_dim).to(self.vq.device))
 		self.hidden_state = h
 		self.current_latent = lat
-		self.current_prop = torch.tensor(prop[:17], dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+		self.current_prop = torch.tensor(prop[:4], dtype=torch.float32).unsqueeze(0).unsqueeze(0)
 		self.current_render = img
 		representation = torch.cat([self.current_latent.flatten(), self.hidden_state[0].flatten()], dim=-1).cpu().numpy()
-		# print(f'Latent shape: {self.current_latent.shape}')
-		# print(f'Current prop shape: {self.current_prop.shape}')
 		return representation, {}
 
 	def step(self, action,) -> tuple:
@@ -96,8 +91,8 @@ class PusherWrapEnv(gym.Env):
 		action: action to take
 		returns: observation (np.array), reward (float), terminated (bool), truncated (bool), info (dict)
 		'''
-		prop, reward, terminated, truncated, info = self.env.step(action*2)
-		prop = prop[0:17]
+		prop, reward, terminated, truncated, info = self.env.step(action)
+		prop = prop[0:4]
 		img = self.get_img()
 		with torch.no_grad():
 			t_img = self.to_tensor_(img).unsqueeze(0).to(self.vq.device)
@@ -109,8 +104,6 @@ class PusherWrapEnv(gym.Env):
 		self.current_prop = torch.tensor(prop, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
 		self.current_render = img
 		representation = torch.cat([self.current_latent.flatten(), self.hidden_state[0].flatten()], dim=-1).cpu().numpy()
-		# print(f'Latent shape: {self.current_latent.shape}')
-		# print(f'Current prop shape: {self.current_prop.shape}')
 		return (
 			representation, # based on world model
 			reward, # from world model
@@ -120,24 +113,26 @@ class PusherWrapEnv(gym.Env):
 		)
 	
 	def render(self):
-		with torch.no_grad():
-			img = self.vq.decode(self.current_latent).squeeze(0).permute(1, 2, 0).cpu().numpy()
-			img = (img * 255).astype(np.uint8)
-			image = Image.fromarray(img)
-			image = self.get_img() 
-			image_resized = image.resize((512, 512), Image.NEAREST)
-			cv2.imshow('DreamEnv', np.array(image_resized))
-			cv2.waitKey(100)
-			return image_resized
-			#return img
+		if True:
+			with torch.no_grad():
+				img = self.vq.decode(self.current_latent).squeeze(0).permute(1, 2, 0).cpu().numpy()
+				img = (img * 255).astype(np.uint8)
+				image = Image.fromarray(img)
+				image = self.get_img() 
+				image_resized = image.resize((512, 512), Image.NEAREST)
+				cv2.imshow('DreamEnv', np.array(image_resized))
+				cv2.waitKey(100)
+				return image_resized
+		else:
+			return self.current_render
 		
 	def close(self):
 		self.env.close()
 		pass
 
 def generate_data(vq:VQVAE, lstm:LSTMQuantized, n_sample:int=1000, policy:BaseAlgorithm=None, training_set:bool=True, round:int=0):
-	base_images_path = 'data/pusher/' + ('tr/' if training_set else 'vl/') + f'round_{round}/'
-	action_path = base_images_path + 'action_reward_data.json'
+	base_path = get_data_path(CURRENT_ENV['img_dir'], training_set, round)
+	action_path = CURRENT_ENV['transitions']
 	actions = []
 	rewards = []
 	proprioception = []
@@ -147,10 +142,11 @@ def generate_data(vq:VQVAE, lstm:LSTMQuantized, n_sample:int=1000, policy:BaseAl
 			actions = f['actions']
 			rewards = f['reward']
 			proprioception = f['proprioception']
-	if not os.path.exists(base_images_path):
-		os.makedirs(base_images_path)
+	if not os.path.exists(base_path):
+		os.makedirs(base_path)
+		os.makedirs(CURRENT_ENV['models'])
 		
-	env = PusherWrapEnv(vq, lstm)
+	env = MetaWrapEnv(vq, lstm)
 	obs, _ = env.reset()
 	step = 0
 	episode = len(actions)
@@ -158,7 +154,7 @@ def generate_data(vq:VQVAE, lstm:LSTMQuantized, n_sample:int=1000, policy:BaseAl
 	actions.append([])
 	rewards.append([])
 	proprioception.append([env.current_prop.flatten().tolist()])
-	env.current_render.save(base_images_path + f'img_{episode}_{step}.png')
+	env.current_render.save(base_path + f'img_{episode}_{step}.png')
 	#for i in tqdm(range(n_sample)):
 	for i in range(n_sample):
 		step += 1
@@ -170,7 +166,7 @@ def generate_data(vq:VQVAE, lstm:LSTMQuantized, n_sample:int=1000, policy:BaseAl
 		obs, rew, ter, trunc, _ = env.step(action)
 		proprioception[-1].append(env.current_prop.flatten().tolist())
 		actions[-1].append(action.tolist())
-		env.current_render.save(base_images_path + f'img_{episode}_{step}.png')
+		env.current_render.save(base_path + f'img_{episode}_{step}.png')
 		rewards[-1].append(float(rew))
 		if ter or trunc:
 			obs, info = env.reset()
@@ -178,7 +174,7 @@ def generate_data(vq:VQVAE, lstm:LSTMQuantized, n_sample:int=1000, policy:BaseAl
 				episode += 1
 				step = 0
 				proprioception.append([env.current_prop.flatten().tolist()])
-				env.current_render.save(base_images_path + f'img_{episode}_{step}.png')
+				env.current_render.save(base_path + f'img_{episode}_{step}.png')
 				actions.append([])
 				rewards.append([])
 	with open(action_path, "w") as f:
@@ -193,21 +189,20 @@ def generate_data(vq:VQVAE, lstm:LSTMQuantized, n_sample:int=1000, policy:BaseAl
 		)
 
 if __name__ == "__main__":
-	SMOOTH = True
-	KL = False
-	vq = load_vq_vae(PUSHER, 64, 16, 4, True, SMOOTH, best_device())
-	lstm = load_lstm_quantized(PUSHER, vq, best_device(), 1024, SMOOTH, False, KL)
-	env = PusherWrapEnv(vq, lstm)
+	SMOOTH = True if SMOOTH > 0 else False
+	vq = load_vq_vae(CURRENT_ENV, CODEBOOK_SIZE, CODE_DEPTH, LATENT_DIM, True, SMOOTH, best_device())
+	lstm = load_lstm_quantized(CURRENT_ENV, vq, best_device(), HIDDEN_DIM, SMOOTH, False, False)
+	env = MetaWrapEnv(vq, lstm)
 	observation, _ = env.reset()
 	frames = []
 	frames.append(env.render())
 	done = False
 	total_reward = 0
 	step_count = 0
-	agent = PPO.load(PUSHER['models'] + 'agent', env)
+	#agent = PPO.load(CURRENT_ENV['models'] + 'agent', env)
 	while not done:
-		#action = env.action_space.sample()  # random action
-		action, _states = agent.predict(observation, deterministic=True)
+		action = env.action_space.sample()  # random action
+		#action, _states = agent.predict(observation, deterministic=True)
 		observation, reward, terminated, truncated, info = env.step(action)
 		print(f"Step {step_count} Reward: {reward}")
 		frames.append(env.render())
