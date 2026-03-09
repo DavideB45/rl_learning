59e9ddf8053da052dd3f6f5a4e4aeddd93c142a7
DavideB45 <dadewww@gmail.com>
2026-03-09 17:40:41 +0100

    Create times.py

---
+++ Benchmark/times.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+	import time
+	tot = 0
+	times = {
+	"collecting_time": 28281.488332509995,
+	"vq_training_time": 12017.390539646149,
+	"lstm_training_time": 8958.13176369667,
+	"dataset_generation_time": 16943.998064756393,
+	"agent_training_time": 6480.0694744586945,
+	"evaluation_time": 18568.107147216797
+	}
+	for key in times:
+		tot += times[key]
+		print(f"{key} : {time.strftime('%H:%M:%S', time.gmtime(times[key]))}")
+	print(f"tot : {time.strftime('%H:%M:%S', time.gmtime(tot))}")
