3aa9701a54253d761d4995dd5d1764e3edc5c390
DavideB45 <d.borghini@studenti.unipi.it>
2026-03-11 12:00:45 +0100

    recon error vq

--- Benchmark/analysis/a_plot.py
+++ Benchmark/analysis/a_plot.py
@@ -13,18 +13,22 @@ import subprocess
 #     shell=True
 # ).split()]
 #data = [1 if x == "b'True'" else 0 for x in data]
-data = [float(x.split('%')[0]) for x in subprocess.check_output(
-    'grep -e "Accurac" data/drawer-open/numbers/3_nosmooth/log3.out | cut -d "|" -f4',
+# data = [float(x.split('%')[0]) for x in subprocess.check_output(
+#     'grep -e "Accurac" data/drawer-open/numbers/3_nosmooth/log3.out | cut -d "|" -f4',
+#     shell=True
+# ).decode().split()]
+data = [float(x.split('\x1b[0m')[0]) for x in subprocess.check_output(
+    'grep -e "recon_loss" data/drawer-open/numbers/1_strike/log3.out | cut -d ":" -f3',
     shell=True
 ).decode().split()]
 print(data)
 # Convert to pandas Series
 min_ = 0
-max_ = 100
+max_ = 300
 series = pd.Series(data)
 
 # Compute rolling mean (window size = 3)
-rolling_mean = series.rolling(window=20).mean()
+rolling_mean = series.rolling(window=10).mean()
 
 # Plot only the rolling mean
 plt.plot(rolling_mean)
@@ -32,4 +36,4 @@ plt.ylim(min_, max_)
 plt.title("Rolling Mean")
 plt.xlabel("Index")
 plt.ylabel("Rolling Mean")
-plt.savefig('lstm_rew_tanh.png')
+plt.savefig('vq_rec_default.png')
