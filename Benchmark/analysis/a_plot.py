39edeb498453181dc644a97ea0e899e2323ca8be
DavideB45 <d.borghini@studenti.unipi.it>
2026-03-11 11:32:25 +0100

    some plots

---
+++ Benchmark/analysis/a_plot.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import subprocess
+
+#data=[float(x) for x in subprocess.check_output("grep , res.csv | tail n +1 |awk -F',' '{print $1}'", shell=True).decode().split()]
+# data = [float(x) for x in subprocess.check_output(
+#     "tail -n +2 res.csv | cut -d',' -f1",
+#     shell=True
+# ).decode().split()]
+# data = [str(x) for x in subprocess.check_output(
+#     "tail -n +2 res.csv | cut -d',' -f2",
+#     shell=True
+# ).split()]
+#data = [1 if x == "b'True'" else 0 for x in data]
+data = [float(x.split('%')[0]) for x in subprocess.check_output(
+    'grep -e "Accurac" data/drawer-open/numbers/3_nosmooth/log3.out | cut -d "|" -f4',
+    shell=True
+).decode().split()]
+print(data)
+# Convert to pandas Series
+min_ = 0
+max_ = 100
+series = pd.Series(data)
+
+# Compute rolling mean (window size = 3)
+rolling_mean = series.rolling(window=20).mean()
+
+# Plot only the rolling mean
+plt.plot(rolling_mean)
+plt.ylim(min_, max_)
+plt.title("Rolling Mean")
+plt.xlabel("Index")
+plt.ylabel("Rolling Mean")
+plt.savefig('lstm_rew_tanh.png')
